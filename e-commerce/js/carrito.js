cb8be64cef26287b9e3bd7766f895efa51d25c49
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-06-16 01:31:25 -0300

    eliminar productos del carrito funcionando

--- e-commerce/js/carrito.js
+++ e-commerce/js/carrito.js
@@ -1,10 +1,15 @@
-const productosEnCarrito=JSON.parse(localStorage.getItem("productos-en-carrito"))
+let productosEnCarrito=localStorage.getItem("productos-en-carrito")
+   productosEnCarrito=JSON.parse(productosEnCarrito)
 const contenedorCarritoVacio=document.querySelector("#carrito-vacio")
 const contenedorCarritoProductos=document.querySelector("#carrito-productos")
 const contenedorCarritoAcciones=document.querySelector("#carrito-acciones")
 const contenedorCarritoComprado=document.querySelector("#carrito-comprado")
+let productosEliminar= document.querySelectorAll(".carrito-producto-eliminar")
 
-if(productosEnCarrito){
+function cargarProductosCarrito(){
+if(productosEnCarrito && productosEnCarrito.length>0){
+
+ 
     contenedorCarritoVacio.classList.add("disabled")
     contenedorCarritoProductos.classList.remove("disabled")
     contenedorCarritoAcciones.classList.remove("disabled")
@@ -38,5 +43,29 @@ if(productosEnCarrito){
     })
    
 }else{
+    contenedorCarritoVacio.classList.remove("disabled")
+    contenedorCarritoProductos.classList.add("disabled")
+    contenedorCarritoAcciones.classList.add("disabled")
+    contenedorCarritoComprado.classList.add("disabled")
 
+}actualizarBotonesEliminar()
+}
+cargarProductosCarrito()
+
+
+
+function actualizarBotonesEliminar(){
+    botonesEliminar=document.querySelectorAll(".carrito-producto-eliminar")
+
+    botonesEliminar.forEach(boton=>{
+    boton.addEventListener("click",eliminarDelCarrito)
+    })
+}
+function eliminarDelCarrito(e){
+    const idBoton=e.currentTarget.id;
+    const index=productosEnCarrito.findIndex(producto=>producto.id===idBoton)
+    productosEnCarrito.splice(index,1)
+    cargarProductosCarrito()
+
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito))
 }
