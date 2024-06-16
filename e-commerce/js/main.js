c28f907fd06fe4be11fae8b799329a6d4a2012f4
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-06-15 23:23:27 -0300

    ver la cantidad de productos agregados al carrito

--- e-commerce/js/main.js
+++ e-commerce/js/main.js
@@ -188,6 +188,9 @@ const productos = [
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar=document.querySelectorAll(".producto-agregar")
+const numerito=document.querySelector("#numerito")
+
 function cargarProductos(productosElegidos) {
   contenedorProductos.innerHTML = "";
   productosElegidos.forEach((producto) => {
@@ -203,6 +206,7 @@ function cargarProductos(productosElegidos) {
             `;
     contenedorProductos.append(div);
   });
+  actualizarBotonesAgregar()
 }
 cargarProductos(productos);
 
@@ -224,3 +228,28 @@ botonesCategorias.forEach((boton) => {
     }
   });
 });
+function actualizarBotonesAgregar(){
+    botonesAgregar=document.querySelectorAll(".producto-agregar")
+    botonesAgregar.forEach(boton=>{
+        boton.addEventListener("click", agregarAlCarrito)
+    })
+}
+const productosEnCarrito=[]
+function agregarAlCarrito(e){
+    const idBoton= e.currentTarget.id
+    const productoAgregado=productos.find(producto =>producto.id===idBoton)
+
+    if(productosEnCarrito.some(producto => producto.id===idBoton)){
+        const index=productosEnCarrito.findIndex(producto=>producto.id===idBoton)
+        productosEnCarrito[index].cantidad++;
+    }else{
+        productoAgregado.cantidad=1
+         productosEnCarrito.push(productoAgregado)
+    }
+    actualizarNumerito();
+   localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito))
+}
+function actualizarNumerito(){
+    let nuevoNumerito=productosEnCarrito.reduce((acc,producto)=> acc + producto.cantidad,0)
+    numerito.innerText=nuevoNumerito
+}
