fc0ec424ea6d2f6c06a6bfc2738baf7060f67a49
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-01 01:58:59 -0300

    añadidas funciones displayMobile y display desktop

--- reserva_hotelera/src/components/Header.js
+++ reserva_hotelera/src/components/Header.js
@@ -1,11 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {AppBar, makeStyles, Toolbar} from "@material-ui/core"
+import logo from "../images/logo.png"
 const Header = () => {
+
+  const [mobile,setMobile]=useState(false)
+  const classes=usestyle()
+  const displayMobile=()=>{}
+  const displayDesktop=()=>(
+    <Toolbar className={classes.toolbar}>
+      <img src={logo} className={classes.logo}/>
+    </Toolbar>
+  )
+      
   return (
-    <div>
-        <h1>Encabezamiento</h1>
-    </div>
+    <AppBar>
+      {
+        mobile ? displayMobile(): displayDesktop()
+      }
+    </AppBar>
   )
 }
+const usestyle=makeStyles((theme)=>({
+  toolbar:{
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+  logo:{
+    height:"45px",
+    margin:theme.spacing(1,0,0,2),
+    objectFit:"contain",
+  }
+}))
 
 export default Header
