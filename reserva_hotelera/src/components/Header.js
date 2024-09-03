5b42bdb5136b1db0e5148ab09eeb23ec768271e4
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-03 00:40:39 -0300

    barra de busqueda displayDesktop

--- reserva_hotelera/src/components/Header.js
+++ reserva_hotelera/src/components/Header.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
-import {AppBar, makeStyles, Toolbar} from "@material-ui/core"
+import {AppBar, InputBase, makeStyles, Toolbar} from "@material-ui/core"
 import logo from "../images/logo.png"
+import SearchIcon from "@material-ui/icons/Search"
 const Header = () => {
 
   const [mobile,setMobile]=useState(false)
-  const classes=usestyle()
+  const classes=useStyle()
   const displayMobile=()=>{}
   const displayDesktop=()=>(
     <Toolbar className={classes.toolbar}>
       <img src={logo} className={classes.logo}/>
+      <div className={classes.center}>
+        <InputBase fullWidth placeholder='Search here...' inputProps={{className:classes.input}}/>
+        <SearchIcon/>
+      </div>
+     
+
     </Toolbar>
   )
       
@@ -20,7 +27,7 @@ const Header = () => {
     </AppBar>
   )
 }
-const usestyle=makeStyles((theme)=>({
+const useStyle=makeStyles((theme)=>({
   toolbar:{
     display:"flex",
     justifyContent:"space-between",
@@ -30,7 +37,20 @@ const usestyle=makeStyles((theme)=>({
     height:"45px",
     margin:theme.spacing(1,0,0,2),
     objectFit:"contain",
-  }
+  },
+  center:{
+    display:"flex",
+    alignItems:"center",
+    padding:theme.spacing(1),
+  },
+  input:{
+    fontSize:"1.2rem",
+    border:"1px solid lightgray",
+    minWidth:"300px",
+    borderRadius:"999px",
+    padding:theme.spacing(1,5,1,5),
+    margin:theme.spacing(1,5,1,5),
+  },
 }))
 
 export default Header
