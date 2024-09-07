bbdbc51d48de9e156d1d8231265f0e696a75c779
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-06 23:11:01 -0300

    integración de Material-UI con CssBaseline, DatePicker, Banner, y RoomCard

--- reserva_hotelera/src/components/Home.js
+++ reserva_hotelera/src/components/Home.js
@@ -1,11 +1,33 @@
+import { CssBaseline, makeStyles } from "@material-ui/core";
 import React from "react";
+import Banner from "./Banner";
+import RoomCard from "./RoomCard";
+import DatePicker from "./DatePicker";
 
 const Home = () => {
+  const classes=useStyle()
   return (
-    <div>
-      <h1>Home page</h1>
+    <>
+    <CssBaseline/>
+    <div className={classes.root}>
+      <div className={classes.dates}>
+        <DatePicker/>
+      </div>
+      <Banner/>
+      <div className={classes.section}>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+        <RoomCard/>
+      </div>
     </div>
+    </>
   );
 };
 
+const useStyle=makeStyles((theme)=>({
+  root:{
+
+  },
+}))
 export default Home;
