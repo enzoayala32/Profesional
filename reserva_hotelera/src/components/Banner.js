de6dee1de000904fa7dae675396c1387468b8514
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-08 23:37:00 -0300

    feat: agregar imagen de fondo y botón estilizado al componente Banner

--- reserva_hotelera/src/components/Banner.js
+++ reserva_hotelera/src/components/Banner.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Button, makeStyles, Typography } from '@material-ui/core'
+import Background_image from "../images/Banner.jpg"
 
 const Banner = () => {
+  const classes=useStyle()
   return (
-    <h2> Soy un Banner</h2>
+    <div className={classes.root}>
+      <div className={classes.info}>
+      <Typography variant='h2'>Plan your getaway....</Typography>
+      <Button variant='contained'>Check our rooms</Button>
+      </div>
+
+    </div>
   )
 }
 
+
+
+const useStyle=makeStyles((theme)=>({
+  root:{
+    height:"50vh",
+    position:"relative",
+    backgroundImage:`url(${Background_image})`,
+    backgroundPosition:"center",
+    backgroundSize:"cover",
+    backgroundRepeat:"no-repeat"
+  },
+  info:{
+    backgroundColor:"#000",
+    color:"#fff",
+    width:"350px",
+    padding:theme.spacing(2),
+    "& h2":{
+      marginBottom:theme.spacing(4)
+    },
+    "& button":{
+      backgroundColor:"rgba(255,103,31,0.4)",
+      color:"#fff",
+      textTransform:"inherit",
+      fontSize:"1.2rem",
+      fontWeight:"bold",
+    },
+    "& button:hover":{
+      backgroundColor:"#fff",
+      color:"rgba(255,103,31,0.4)",
+    }
+  }
+}))
 export default Banner
