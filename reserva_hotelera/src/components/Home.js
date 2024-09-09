94cf2cf9237b2c9a693a650d8501987b04de0301
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-09 02:27:41 -0300

    feat: agregar funcionalidad de mostrar/ocultar DatePicker

--- reserva_hotelera/src/components/Home.js
+++ reserva_hotelera/src/components/Home.js
@@ -1,18 +1,29 @@
-import { CssBaseline, makeStyles } from "@material-ui/core";
-import React from "react";
+import { Button, CssBaseline, makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
 import Banner from "./Banner";
 import RoomCard from "./RoomCard";
 import DatePicker from "./DatePicker";
 
 const Home = () => {
   const classes=useStyle()
+  const [showdates,setShowdates]=useState(false)
   return (
     <>
     <CssBaseline/>
     <div className={classes.root}>
       <div className={classes.dates}>
-        <DatePicker/>
+        <Button onClick={()=>setShowdates(!showdates)}>
+          {
+          showdates ? "Hide": "Search dates"
+          }
+          </Button>
+       
+        
       </div>
+      {
+       showdates && <DatePicker/>
+      }
+
       <Banner/>
       <div className={classes.section}>
         <RoomCard/>
@@ -27,7 +38,24 @@ const Home = () => {
 
 const useStyle=makeStyles((theme)=>({
   root:{
-
+    display:"flex",
+    flexDirection:"column"
+  },
+  dates:{
+    display:"flex",
+    flexDirection:"column",
+    "& button":{
+      border:"1px solid #ccc",
+      backgroundColor:"#fff",
+      color:"rgba(255,103,31,0.4)",
+      textTransform:"inherit",
+      fontSize:"1.2rem",
+      fontWeight:"bold"
+    },
+    "& button:hover":{
+      backgroundColor:"rgba(255,103,31,0.4)",
+      color:"#fff",
+    }
   },
 }))
 export default Home;
