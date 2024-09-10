21c3031846b0b63ab64c27dc5565f50ca29aa7ef
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-09 23:41:15 -0300

    integrar Material-UI, agregar manejo de imágenes y mejorar el diseño con estilos responsivos

--- reserva_hotelera/src/components/RoomCard.js
+++ reserva_hotelera/src/components/RoomCard.js
@@ -1,9 +1,44 @@
+import { makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
 
-const RoomCard = () => {
+const RoomCard = ({src,title,description}) => {
+  const classes=useStyle()
   return (
-    <h3> soy un RoomCard</h3>
+    <div className={classes.root}> 
+    <figure className={classes.imageWrapper}>
+      <img className={classes.media} src={src} alt={title}/>
+    </figure>
+    <Typography variant='h5' color='textPrimary'>{title}</Typography>
+    <Typography variant='h6' color='textSecondary'>{description}</Typography>
+    </div>
+    
   )
 }
 
+const useStyle=makeStyles((theme)=>({
+  root:{
+    margin:theme.spacing(4,0,2,0),
+    [theme.breakpoints.down("sm")]: {
+      "& h6":{
+        wordWrap:"break-word"
+      }
+    }
+  },
+  imageWrapper:{
+    overflow:"hidden"
+  },
+  media:{
+    height:"400px",
+    width:"600px",
+    objectFit:"cover",
+    transform:"scale(1.1)",
+    WebkitTransform:"scale(1.1)",
+    WebkitTransition:".4s ease-in-out", 
+    transition:".4s ease-in-out", 
+    "&:hover":{
+      transform:"scale(1)",
+      WebkitTransform:"scale(1.1)",
+    }
+  }
+}))
 export default RoomCard
