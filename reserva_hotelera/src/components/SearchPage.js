e2f2617d2b327f1b4e9423185c07aeff602f3ee6
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-12 22:24:58 -0300

    feat: añadir filtro y selector de precios
    - Añadidos Chip, Slider y Typography.
    - Filtro con chips dinámicos e icono de eliminación.
    - Selector de precios con Slider.
    - Estilos personalizados con makeStyles.

--- reserva_hotelera/src/components/SearchPage.js
+++ reserva_hotelera/src/components/SearchPage.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import { Chip, makeStyles, Slider, Typography } from '@material-ui/core'
+import { chips } from '../mockData'
+import { HighlightOffTwoTone } from '@material-ui/icons'
 
 const SearchPage = () => {
+  const classes=useStyle()
   return (
-    <div>
-        <h1>searchPage</h1>
+    <div className={classes.root}>
+        <Typography variant='h5' gutterBottom>Avaibable rooms </Typography>
+        <div className={classes.chips}>
+          {
+            chips.map(({key,label})=>{
+              let icon=<HighlightOffTwoTone/>
+              return(
+                <Chip label={label}
+                 key={key}
+                  icon={icon} 
+                  className={classes.chip}/>
+              )
+            })
+          }
+        </div>
+        <div className={classes.selector}>
+          <Typography gutterBottom>Prices</Typography>
+            <Slider 
+            aria-labelledby="continuous-slider"
+            min={100}
+            max={400}
+            valueLabelDisplay='auto'
+            color='secondary'/>
+        </div>
     </div>
   )
 }
+
+const useStyle=makeStyles((theme)=>({
+  root:{
+      
+  },
+  chips:{
+    padding:theme.spacing(1)
+  },
+  chip:{
+    margin:theme.spacing(1)
+  },
+  selector:{
+    width:"300px",
+    marginLeft:theme.spacing(3),
+  },
+}))
 
 export default SearchPage
