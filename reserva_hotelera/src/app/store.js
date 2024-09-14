05c5704a07729c96acae454c6a710750dab9f7a6
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-09-14 02:00:43 -0300

    Agregar reducers de inicio y fin

--- reserva_hotelera/src/app/store.js
+++ reserva_hotelera/src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import startReducer from "../features/counter/startSlice"
+import endReducer from "../features/counter/endSlice"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    start:startReducer,
+    end:endReducer,
   },
 });
