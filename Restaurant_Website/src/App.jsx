d0bd5db07b57179a128c230fc01a8843bbe7066a
Enzo Ayala <ayalaenzoenrique2003@gmail.com>
2024-10-19 03:12:00 -0300

    Inicio proyecto web de restaurante: subida de componentes, estilos e imágenes

---
+++ Restaurant_Website/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Menu from './sections/Menu'
+import Items from './sections/Items'
+import Banner from './sections/Banner'
+import Contact from './sections/Contact'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Menu/>
+    <Items/>
+    <Banner/>
+    <Contact/>
+    </>
+  )
+}
+
+export default App
